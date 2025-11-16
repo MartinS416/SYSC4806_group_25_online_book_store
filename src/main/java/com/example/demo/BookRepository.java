7b67ca58c9924d40322e40b352125d854b5836cc
Yuvraj Bains <128195694+Yuvi-Pain@users.noreply.github.com>
2025-11-16 13:21:38 -0500

    Add searchBooks method to BookRepository
    Added a custom query method to search books by title, author, or category.

--- src/main/java/com/example/demo/BookRepository.java
+++ src/main/java/com/example/demo/BookRepository.java
@@ -1,6 +1,14 @@
 package com.example.demo;
 
-import com.example.demo.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import java.util.List;
 
-public interface BookRepository extends JpaRepository<Book, Long> {}
+public interface BookRepository extends JpaRepository<Book, Long> {
+
+    @Query("SELECT b FROM Book b WHERE " +
+           "LOWER(b.title) LIKE LOWER(CONCAT('%', :keyword, '%')) OR " +
+           "LOWER(b.author) LIKE LOWER(CONCAT('%', :keyword, '%')) OR " +
+           "LOWER(b.category) LIKE LOWER(CONCAT('%', :keyword, '%'))")
+    List<Book> searchBooks(String keyword);
+}
