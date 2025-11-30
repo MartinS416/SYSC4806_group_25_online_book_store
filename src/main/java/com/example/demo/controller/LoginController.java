2b6cd1d4954296c43da1364feb362df63aeaa1e6
martinskach <martin.skachkov@me.com>
2025-11-30 17:23:59 -0500

    Added profile page which allows updating information and password

--- src/main/java/com/example/demo/controller/LoginController.java
+++ src/main/java/com/example/demo/controller/LoginController.java
@@ -1,23 +1,36 @@
 package com.example.demo.controller;
 
 import com.example.demo.model.Customer;
+import com.example.demo.model.PasswordResetToken;
+import com.example.demo.repository.CustomerRepository;
 import com.example.demo.service.LoginService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+import java.util.UUID;
 
 @Controller
 public class LoginController {
     private final LoginService loginService;
+    private final CustomerRepository customerRepo;
 
-    public LoginController(LoginService loginService) {
+
+    @Value("${app.base-url}")
+    private String appBaseUrl;
+
+    public LoginController(LoginService loginService,
+                           CustomerRepository customerRepo) {
         this.loginService = loginService;
+        this.customerRepo = customerRepo;
+
     }
+
 
     /**
      * mapping for login page
